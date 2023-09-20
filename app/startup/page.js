dfce8f877939291b3022b99d92d0c5931caf7c62
Alfonso Altamar <aaltamar12@hotmail.com>
2023-09-20 11:06:21 -0500

    feat: remove tailwindcss-safe-area and refactor Startup

--- app/startup/page.js
+++ app/startup/page.js
@@ -17,9 +17,7 @@ export default function StartUp({ inter, provider }) {
     <body>
       <provider>
         <div className="absolute inset-0 flex items-center justify-center bg-gray-600">
-          <div
-            className={`absolute inset-0 text-center ${style.backgroundAdMedia}`}
-          >
+          <div className={`absolute inset-0 ${style.backgroundAdMedia}`}>
             <video
               autoPlay
               loop
@@ -33,38 +31,45 @@ export default function StartUp({ inter, provider }) {
           </div>
 
           <div
-            className="grid grid-rows-2 text-center items-center justify-center gap-48"
+            className="grid grid-rows-2 text-center items-center justify-center"
             style={{ zIndex: 1 }}
           >
-            <div className="container">
-              <h1
-                className={`p-2 text-white text-startup-h1 ${kanit800.className}`}
-              >
-                AdMedia
-              </h1>
+            <div
+              className="flex h-72 justify-center"
+              style={{ paddingTop: "100px" }}
+            >
+              <div className="container">
+                <h1
+                  className={`p-2 text-white text-startup-h1 ${kanit800.className}`}
+                >
+                  AdMedia
+                </h1>
 
-              <small
-                className={`p-1 text-slate-200 text-startup-small ${kanitItalic200.className}`}
-              >
-                Anunciar nunca fue tan facil
-              </small>
+                <small
+                  className={`p-1 text-slate-200 text-startup-small ${kanitItalic200.className}`}
+                >
+                  Anunciar nunca fue tan facil
+                </small>
+              </div>
             </div>
 
-            <div className="w-64 lg:w-96">
-              <CustomButton
-                className={`text-xl text-white bg-gradient-to-tr from-pink-500 to-orange-400 shadow-md rounded-full h-14 ${kanit200.className}`}
-                size="lg"
-                fullWidth="true"
-                text="Empezar"
-              />
-              <div className="p-2">
-                <Link href={"#"}>
-                  <small
-                    className={`text-white ${kanit500.className} underline`}
-                  >
-                    ¿Eres nuevo? Registrate
-                  </small>
-                </Link>
+            <div className="flex" style={{ paddingTop: "100px" }}>
+              <div className="container w-64 lg:w-96">
+                <CustomButton
+                  className={`text-xl text-white bg-gradient-to-tr from-pink-500 to-orange-400 shadow-md rounded-full h-14 ${kanit200.className}`}
+                  size="lg"
+                  fullWidth="true"
+                  text="Empezar"
+                />
+                <div className="p-2">
+                  <Link href={"#"}>
+                    <small
+                      className={`text-white ${kanit500.className} underline`}
+                    >
+                      ¿Eres nuevo? Registrate
+                    </small>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
